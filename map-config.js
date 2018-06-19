51ca5e0d5560b3b7d4010d712697ba163fa89614
Justin Byrne <jbyrne1030@gmail.com>
2018-06-20 08:59:09 +1000

    Further update styles

--- map-config.js
+++ map-config.js
@@ -6,74 +6,75 @@
 	},
 	'map_1':{
 		'hover': 'AUSTRALIAN CAPITAL TERRITORY',//region hover content
-		'enable':true,//true/false to enable/disable this region
-		'url':'http://www.html5interactivemaps.com/', //Goto URL
+		'enable':false,//true/false to enable/disable this region
+		'url':'#', //Goto URL
 		'target':'same_window', //open link in new window:new_window, open in current window:same_window, or none for nothing.
 		'upColor':'#CFBFB3', //region color when page loads
-		'overColor':'#FFEE88', //region color when mouse hover
-		'downColor':'#993366',//region color when mouse clicking
+		'overColor':'#BD4A3D', //region color when mouse hover
+		'downColor':'#A33124',//region color when mouse clicking
 	},
 	'map_2':{
 		'hover': 'NEW SOUTH WALES',
 		'enable':true,
-		'url':'http://www.html5interactivemaps.com/',
+		'url':'#',
 		'target':'same_window',
 		'upColor':'#CFBFB3',
-		'overColor':'#FFEE88',
-		'downColor':'#993366',
+		'overColor':'#BD4A3D',
+		'downColor':'#A33124',
 	},
 	'map_3':{
 		'hover': 'NORTHERN TERRITORY',
 		'enable':true,
-		'url':'http://www.html5interactivemaps.com/',
+		'url':'#',
 		'target':'same_window',
 		'upColor':'#CFBFB3',
-		'overColor':'#FFEE88',
-		'downColor':'#993366',
+		'overColor':'#BD4A3D',
+		'downColor':'#A33124',
 	},
 	'map_4':{
 		'hover': 'QUEENSLAND',
+		'hover': 'QUEENSLAND',
 		'enable':true,
-		'url':'http://www.html5interactivemaps.com/',
+		'url':'#',
 		'target':'same_window',
 		'upColor':'#CFBFB3',
-		'overColor':'#FFEE88',
-		'downColor':'#993366',
+		'overColor':'#BD4A3D',
+		'downColor':'#A33124',
 	},
 	'map_5':{
 		'hover': 'SOUTH AUSTRALIA',
 		'enable':true,
-		'url':'http://www.html5interactivemaps.com/',
+		'url':'#',
 		'target':'same_window',
 		'upColor':'#CFBFB3',
-		'overColor':'#FFEE88',
-		'downColor':'#993366',
+		'overColor':'#BD4A3D',
+		'downColor':'#A33124',
 	},
 	'map_6':{
 		'hover': 'TASMANIA',
 		'enable':true,
-		'url':'http://www.html5interactivemaps.com/',
+		'url':'#',
 		'target':'same_window',
 		'upColor':'#CFBFB3',
-		'overColor':'#FFEE88',
-		'downColor':'#993366',
+		'overColor':'#BD4A3D',
+		'downColor':'#A33124',
 	},
 	'map_7':{
 		'hover': 'VICTORIA',
 		'enable':true,
-		'url':'http://www.html5interactivemaps.com/',
+		'url':'#',
 		'target':'same_window',
 		'upColor':'#CFBFB3',
-		'overColor':'#FFEE88',
-		'downColor':'#993366',
+		'overColor':'#BD4A3D',
+		'downColor':'#A33124',
 	},
 	'map_8':{
 		'hover': 'WESTERN AUSTRALIA',
 		'enable':true,
-		'url':'http://www.html5interactivemaps.com/',
+		'url':'#',
 		'target':'same_window',
 		'upColor':'#CFBFB3',
-		'overColor':'#FFEE88',
-		'downColor':'#993366',
+		'overColor':'#BD4A3D',
+		'downColor':'#A33124',
 	},
 }
