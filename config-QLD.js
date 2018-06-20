639d50512a6369a87b13b17f0c25ecd183a6778c
Justin Byrne <jbyrne1030@gmail.com>
2018-06-20 10:10:47 +1000

    Highlight each state in a separate JS file

---
+++ config-QLD.js
@@ -0,0 +1,80 @@
+var map_config = {
+	'default':{
+		'borderColor':'#F4F0EC', //regions borders
+		'namesColor':'#FFFFFF', //short names
+		'mapShadow':'transparent', //shadow color below the map, can be 'transparent'
+	},
+	'map_1':{
+		'hover': 'AUSTRALIAN CAPITAL TERRITORY',//region hover content
+		'enable':true,//true/false to enable/disable this region
+		'url':'#', //Goto URL
+		'target':'same_window', //open link in new window:new_window, open in current window:same_window, or none for nothing.
+		'upColor':'#CFBFB3', //region color when page loads
+		'overColor':'#BD4A3D', //region color when mouse hover
+		'downColor':'#A33124',//region color when mouse clicking
+	},
+	'map_2':{
+		'hover': 'NEW SOUTH WALES',
+		'enable':true,
+		'url':'#',
+		'target':'same_window',
+		'upColor':'#CFBFB3',
+		'overColor':'#BD4A3D',
+		'downColor':'#A33124',
+	},
+	'map_3':{
+		'hover': 'NORTHERN TERRITORY',
+		'enable':true,
+		'url':'#',
+		'target':'same_window',
+		'upColor':'#CFBFB3',
+		'overColor':'#BD4A3D',
+		'downColor':'#A33124',
+	},
+	'map_4':{
+		'hover': 'QUEENSLAND',
+		'hover': 'QUEENSLAND',
+		'enable':true,
+		'url':'#',
+		'target':'same_window',
+		'upColor':'#BD4A3D',
+		'overColor':'#BD4A3D',
+		'downColor':'#BD4A3D',
+	},
+	'map_5':{
+		'hover': 'SOUTH AUSTRALIA',
+		'enable':true,
+		'url':'#',
+		'target':'same_window',
+		'upColor':'#CFBFB3',
+		'overColor':'#BD4A3D',
+		'downColor':'#A33124',
+	},
+	'map_6':{
+		'hover': 'TASMANIA',
+		'enable':true,
+		'url':'#',
+		'target':'same_window',
+		'upColor':'#CFBFB3',
+		'overColor':'#BD4A3D',
+		'downColor':'#A33124',
+	},
+	'map_7':{
+		'hover': 'VICTORIA',
+		'enable':true,
+		'url':'#',
+		'target':'same_window',
+		'upColor':'#CFBFB3',
+		'overColor':'#BD4A3D',
+		'downColor':'#A33124',
+	},
+	'map_8':{
+		'hover': 'WESTERN AUSTRALIA',
+		'enable':true,
+		'url':'#',
+		'target':'same_window',
+		'upColor':'#CFBFB3',
+		'overColor':'#BD4A3D',
+		'downColor':'#A33124',
+	},
+}
